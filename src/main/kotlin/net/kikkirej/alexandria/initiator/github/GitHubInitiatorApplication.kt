2c41733ad457369bd347bf7826cf288eee8ecd97
Kikkirej <5462557+Kikkirej@users.noreply.github.com>
2022-02-05 16:59:31 +0100

    🐛

--- src/main/kotlin/net/kikkirej/alexandria/initiator/github/GitHubInitiatorApplication.kt
+++ src/main/kotlin/net/kikkirej/alexandria/initiator/github/GitHubInitiatorApplication.kt
@@ -2,8 +2,10 @@ package net.kikkirej.alexandria.initiator.github
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class GitHubInitiatorApplication
 
 fun main(args: Array<String>) {
